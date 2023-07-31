e4e0f56559c7a2fa65c5cc66679338b83ab31d41
May Tusek <tusek.may@gmail.com>
2023-07-31 00:34:05 -0700

    Fixes generation and removes commited binaries.

--- binaries/generate.sh
+++ binaries/generate.sh
@@ -5,26 +5,29 @@ cwd=$(pwd)
 cd ../gen/src && yarn build
 cd $cwd
 
-ln -s ../gen/bin/gen.js ./bin/
+ln -s ../gen/bin/gen.js ./
+mv ./gen.js ./bin/
 
 # Build the Windows "binary".
 cwd=$(pwd)
 cd ./winblows
 
-tee ./gen <<EOF
+cat <<EOF > gen.ps1
 # Join the arguments into a single string with spaces in between
-$arguments = $args -join ' '
+\$arguments = \$args -join ' '
 
 # Define the path to the node.js executable and the gen.js script
-$nodePath = "node"
-$genScriptPath = "../bin/gen.js"
+\$nodePath = "node"
+\$genScriptPath = "\$PSScriptRoot/../../gen/bin/gen.js"
 
 # Build the full command to execute
-$fullCommand = "$nodePath $genScriptPath $arguments"
+\$fullCommand = "\$nodePath \$genScriptPath \$arguments"
 
 # Execute the command and capture the output
-$output = Invoke-Expression -Command $fullCommand
+\$output = Invoke-Expression -Command \$fullCommand
 
 # Output the result
-Write-Output $output
+Write-Output \$output
 EOF
+
+cd $cwd
